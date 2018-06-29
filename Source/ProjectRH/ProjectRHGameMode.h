e6d2a8f65714ac8f3778735756eb37c74aa86329
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2018-06-29 22:50:34 +0200

    Initial commit

---
+++ Source/ProjectRH/ProjectRHGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRHGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectRHGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectRHGameMode();
+};
+
+
+
