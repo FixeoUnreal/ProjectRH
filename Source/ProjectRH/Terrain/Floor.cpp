b2a8a1f50eb42cf51ecdeadd295f3d670661a45a
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2018-07-12 10:44:16 +0200

    Create first map: Halo, which looks like a loop

---
+++ Source/ProjectRH/Terrain/Floor.cpp
@@ -0,0 +1,27 @@
+// Copyright 2018 , Hoang Giang Phi, All Rights Reserved.
+
+#include "Floor.h"
+
+
+// Sets default values
+AFloor::AFloor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ProjectRH/Terrain/Floor.h
@@ -0,0 +1,28 @@
+// Copyright 2018 , Hoang Giang Phi, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Floor.generated.h"
+
+UCLASS()
+class PROJECTRH_API AFloor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
